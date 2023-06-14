b08609a9a1ab4f04e39597187fec4174e0fcb062
Vivien <vivientopou57@gmail.com>
2023-06-14 17:22:52 +0000

    commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('prenom')->nullable();
+            $table->string('prenoms')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('cv')->nullable();
+            $table->string('libelle')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
