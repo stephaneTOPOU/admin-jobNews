884767cf973515585407e70871de453c530750c4
Hugues658 <tekoaffohugues@gmail.com>
2023-08-07 15:50:18 +0000

    commit

--- resources/views/frontend/header/header1.blade.php
+++ resources/views/frontend/header/header1.blade.php
@@ -9,7 +9,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>JobNews</title>
-    <meta name="description" content="JobNews">
+    <title>Admin-JobsActu</title>
+    <meta name="description" content="La plateforme des opportunités du TOGO (offres d'emploi togo, offres de stages)">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @include('sweetalert::alert')
